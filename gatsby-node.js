0c96c1de00e1697ce81cd26f138e4b04b72deeea
krzysiek <krzysiek@KrzysztofsMBP2.lan>
2022-08-10 23:49:57 +0200

    Add gatsby-node file. Source data from api. Pull that data into gatsby data layer. Remove fetching that data from page /map. Refactor. Describe next todo at /map

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,44 @@
+const fetch = (...args) =>
+  import(`node-fetch`).then(({ default: fetch }) => fetch(...args));
 
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  try {
+    const result = await fetch(
+      `https://karpteam.herokuapp.com/api/lakes/woj?voivodeship=${"Podkarpackie"}`,
+      {
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        withCredentials: false,
+        credentials: "same-origin",
+        crossdomain: true,
+      }
+    );
+    const resultData = await result.json();
+
+    resultData.forEach((data) => {
+      createNode({
+        // nameWithOwner and url are arbitrary fields from the data
+        name: data.name,
+        city: data.city,
+        voiv: data.voivodeship,
+        // required fields
+        id: String(data.id),
+        parent: null,
+        children: [],
+        internal: {
+          type: `Fishery`,
+          contentDigest: createContentDigest(resultData),
+        },
+      });
+    });
+  } catch (err) {
+    console.log("err...", err);
+  }
+};
--- src/pages/map.js
+++ src/pages/map.js
@@ -1,8 +1,9 @@
+//todos
+//1. get fishery data from gatsby data layer using grapql;
+
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import axios from "axios";
 import { Link } from "gatsby";
-
 import Pin from "../assets/images/pin.svg";
 import Fish from "../assets/images/fish.svg";
 import Trophy from "../assets/images/trophy.svg";
@@ -20,37 +21,37 @@ const Map = () => {
   const handleClick = (e) => {
     setVoiv(e.target.id);
   };
-  const loadLowiskaByVoivodeship = async () => {
-    // get data from API
-    const response = await axios.get(
-      `https://karpteam.herokuapp.com/api/lakes/woj?voivodeship=${voiv}`,
-      {
-        mode: "cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        withCredentials: false,
-        credentials: "same-origin",
-        crossdomain: true,
-      }
-    );
-    console.log("Lowiska Data", response.data);
-    setLowiska(response.data);
-    if (response.data.length === 0) {
-      setError(true);
-    } else {
-      setError(false);
-      ref.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-  useEffect(() => {
-    if (voiv === null) {
-      return;
-    }
-    loadLowiskaByVoivodeship();
-  }, [voiv]);
+  // const loadLowiskaByVoivodeship = async () => {
+  //   // get data from API
+  //   const response = await axios.get(
+  //     `https://karpteam.herokuapp.com/api/lakes/woj?voivodeship=${voiv}`,
+  //     {
+  //       mode: "cors",
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "*",
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //       withCredentials: false,
+  //       credentials: "same-origin",
+  //       crossdomain: true,
+  //     }
+  //   );
+  //   console.log("Lowiska Data", response.data);
+  //   setLowiska(response.data);
+  //   if (response.data.length === 0) {
+  //     setError(true);
+  //   } else {
+  //     setError(false);
+  //     ref.current.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // };
+  // useEffect(() => {
+  //   if (voiv === null) {
+  //     return;
+  //   }
+  //   loadLowiskaByVoivodeship();
+  // }, [voiv]);
 
   return (
     <MapCss>
@@ -228,7 +229,8 @@ const Map = () => {
                       alt="silhouette"
                       src={Silhouette}
                     ></img>
-                    <b>Liczba stanowisk: </b>{lowisko.numberOfPegs || 'nieznana'}
+                    <b>Liczba stanowisk: </b>
+                    {lowisko.numberOfPegs || "nieznana"}
                   </div>
                   <div className="lowi_itm_amnt cena">
                     <img className="dollar" alt="dollar" src={Dollar}></img>
