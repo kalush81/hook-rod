dfce7a77adbf352e9c1b0e905fe607f7255ae7c3
krzysiek <krzysiek@Krzysztofs-MacBook-Pro-2.local>
2023-04-21 22:14:45 +0200

    Fix build time error window is not defined

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -2,21 +2,28 @@ import React, { useState, useEffect } from "react";
 
 const useWindowSize = () => {
   const [size, setSize] = useState(() => {
-    return window.innerWidth;
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    } else {
+      return null;
+    }
   });
 
-  const handleResize = () => {
-    //console.log(window.innerWidth);
-    if (window.innerWidth < 501) return setSize(7);
-    if (window.innerWidth > 500 && window.innerWidth < 601) return setSize(8);
-    if (window.innerWidth > 600 && window.innerWidth < 701) return setSize(9);
-    if (window.innerWidth > 700 && window.innerWidth < 801) return setSize(10);
-    if (window.innerWidth > 800 && window.innerWidth < 901) return setSize(11);
-    if (window.innerWidth > 900 && window.innerWidth < 1001) return setSize(12);
-    if (window.innerWidth > 1000) return setSize(14);
-  };
-
   useEffect(() => {
+    const handleResize = () => {
+      //console.log(window.innerWidth);
+      if (window.innerWidth < 501) return setSize(7);
+      if (window.innerWidth > 500 && window.innerWidth < 601) return setSize(8);
+      if (window.innerWidth > 600 && window.innerWidth < 701) return setSize(9);
+      if (window.innerWidth > 700 && window.innerWidth < 801)
+        return setSize(10);
+      if (window.innerWidth > 800 && window.innerWidth < 901)
+        return setSize(11);
+      if (window.innerWidth > 900 && window.innerWidth < 1001)
+        return setSize(12);
+      if (window.innerWidth > 1000) return setSize(14);
+    };
+
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
