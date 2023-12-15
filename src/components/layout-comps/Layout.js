e58277fcb24f89a3c359053fc537798ea2591d4e
Krzysztof Kaluza <kzysiek@Krzysztofs-MacBook-Pro.local>
2023-12-15 18:53:12 +0100

    Move ant config provider to Layout

--- src/components/layout-comps/Layout.js
+++ src/components/layout-comps/Layout.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../../styles/GlobalStyles';
+import { ConfigProvider } from 'antd';
+import plPL from 'antd/lib/locale/pl_PL';
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <GlobalStyles />
-      <Nav />
-      {children}
+    <ConfigProvider>
+      <div>
+        <GlobalStyles />
+        <Nav />
+        {children}
 
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©2023 Created by Ant UED
-      </Footer>
-    </div>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </div>
+    </ConfigProvider>
   );
 };
 
