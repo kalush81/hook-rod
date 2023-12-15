561ae3cfca513fde5e5113a3d373e0c6d1fbb67e
Krzysztof Kaluza <kzysiek@Krzysztofs-MacBook-Pro.local>
2023-12-15 19:03:02 +0100

    Move global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Layout from "./src/components/layout-comps/Layout";
+import { ConfigProvider } from "antd";
+import plPL from "antd/lib/locale/pl_PL";
+import GlobalStyles from "./src/styles/GlobalStyles";
+
+export function wrapPageElement({ element, props }) {
+  return (
+    //use this config provider to custome style ANT's components
+    <ConfigProvider
+      locale={plPL}
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#00b96b",
+          //borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: "#f6ffed",
+        },
+        components: {
+          DatePicker: {
+            cellHeight: 24,
+          },
+        },
+      }}
+    >
+      <GlobalStyles />
+      <Layout {...props}>{element}</Layout>
+    </ConfigProvider>
+  );
+}
--- src/components/layout-comps/Layout.js
+++ src/components/layout-comps/Layout.js
@@ -2,22 +2,17 @@ import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../../styles/GlobalStyles';
-import { ConfigProvider } from 'antd';
-import plPL from 'antd/lib/locale/pl_PL';
 
 const Layout = ({ children }) => {
   return (
-    <ConfigProvider>
-      <div>
-        <GlobalStyles />
-        <Nav />
-        {children}
-
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©2023 Created by Ant UED
-        </Footer>
-      </div>
-    </ConfigProvider>
+    <>
+      {/* <GlobalStyles /> */}
+      <Nav />
+      {children}
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </>
   );
 };
 
