81c2b5cba4f59df68e21f0cbbc615fa7403db16b
Krzysztof Kaluza <kzysiek@Krzysztofs-MacBook-Pro.local>
2023-12-16 12:43:21 +0100

    Remove gatsby ssr file , import antd/es into Layout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,1 @@
-import React from "react";
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link rel="stylesheet" href="ant/dist/reset.css" key="antd-stylesheet" />,
-  ]);
-};
